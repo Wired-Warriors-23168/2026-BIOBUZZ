45415066631812fad7c7263f72bef8555da4c27c
Wired-Warriors <233858487+maxwellandrewtomas-prog@users.noreply.github.com>
2026-09-21 19:48:50 -0400

    example commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/base.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/base.java
@@ -80,7 +80,7 @@ public class base extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
+            //levi was here
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
